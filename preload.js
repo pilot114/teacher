ce7771af644cbb7d23cc3b5f7b5320e474d8047f
Oleg <chulkov@sibvaleo.com>
2023-04-22 21:25:29 +0200

    migrate to 2023

---
+++ preload.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('chrome', process.versions.chrome)
+    console.log('node', process.versions.node)
+    console.log('electron', process.versions.electron)
+})
